364fd7f57b5166b3125e94a5d336d835957774c2
YeGuanWei <787139598@qq.com>
2021-12-29 10:08:06 +0800

    Initial commit

---
+++ src/main/java/com/ForumApplication.java
@@ -0,0 +1,22 @@
+package com;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumApplication {
+
+    private static Logger log = LogManager.getLogger(ForumApplication.class);
+
+    public static void main(String[] args) {
+        // 启动时间计时
+        long startTime = System.currentTimeMillis();
+        // 启动项目
+        SpringApplication.run(ForumApplication.class, args);
+        // 输出启动时间
+        log.error("启动完成，耗时：" + (System.currentTimeMillis() - startTime) / 1000.0 + "秒");
+    }
+
+}
